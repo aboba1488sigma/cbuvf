a1f1fc74af1d1050c200e5a10a02ad37bcba5fe4
aboba1488sigma <sobaken666yt@gmail.com>
2025-01-18 19:16:50 +0700

    Add files via upload

---
+++ AbabaBaba.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbabaBaba : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       //вывод приветстыие миру
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("crickets");
+    }
+}
