266484ddf44e57fce38b0b394cc9c8258c1c4490
aboba1488sigma <sobaken666yt@gmail.com>
2025-02-15 19:08:09 +0700

    Add files via upload

---
+++ platform.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platform : MonoBehaviour
+{
+   public float speed;
+   public Vector3 direction;
+   public bool isActive;
+   
+   void Update()
+    {
+       if (isActive)
+       {
+       transform.position += direction * Time.deltaTime;
+       }
+    }
+    void OnTriggerEnter(Collider other){
+        if(other.tag == "PlatformStop")
+    {
+        direction *= -1;
+    }
+    if(other.tag == "Player")
+    
+    {
+        isActive = true;
+    }
+}
+    void OnTriggerExit(Collider other){
+        if(other.tag == "Player")
+        {
+            isActive = false;
+        }
+    }
+}
